6a45a10997261be196f246e029ee582d793a7bb8
jihwooon <jihwooon@gmail.com>
2022-11-28 23:50:42 +0900

    버블 정렬을 최적화 하라
    버블 정렬은 정렬이 완료 됐음에도, 배열의 길이에 따라 끝까지 비교하게
    되므로 쓸데없이 시간이 낭비가 된다. 따라서 이를 최적화하는 작업이
    필요하다.
    
    스왑을 안 한다는 것은 이미 정렬이 끝난다는 의미이므로, 끝낸다.

--- BubbleSort/app.test.js
+++ BubbleSort/app.test.js
@@ -1,15 +1,17 @@
 function bubblesSort(arr) {
   
   for(let i = 0; i < arr.length; i++) {
+    noSwaps = true;
     for(let j = 0; j < arr.length; j++) {
       if(arr[j] > arr[j+1]) {
         let temp = arr[j];
         arr[j] = arr[j+1];
         arr[j+1] = temp;
+        noSwaps = false;
       }
     }
+    if(noSwaps) break;
   }
-  
   return arr;
 }
 
