5ea801e94a5de5f352cead1b8bacb7ba536d3b2d
jihwooon <jihwooon@gmail.com>
2022-11-30 21:35:31 +0900

    삽입 정렬을 공부하라
    1. 배열에서 두 번째 요소를 선택하는 것부터 시작
    2. 이제 두 번째 요소를 이전 요소와 비교하고 필요한 경우 스왑합니다.
    3. 다음 요소로 계속 이동하고 순서가 잘못된 경우 정렬된 부분(즉, 왼쪽)을 반복하여 요소를 올바른 위치에 배치합니다.

---
+++ Sort/insertionSort.test.js
@@ -0,0 +1,17 @@
+function insertionSort(arr) {
+    for(var i = 1; i < arr.length; i++) {
+        var currentValue = arr[i];
+        for(var j = i - 1; j >= 0 && arr[j] > currentValue; j--) {
+            arr[j+1] = arr[j]
+        }
+        arr[j+1] = currentValue;
+    }
+
+    return arr;
+}
+
+describe('두 번째 요소를 이전 원소와 비교하고, 조건이 맞지 않으면', () => {
+    it('다음 요소로 계속 이동하고 순서가 잘못된 경우 정렬된 부분(즉, 왼쪽)을 반복하여 요소를 올바른 위치에 배치합니다. ',() => {
+        expect(insertionSort([2,1,9,76,4])).toStrictEqual([1,2,4,9,76]);
+    })
+});
