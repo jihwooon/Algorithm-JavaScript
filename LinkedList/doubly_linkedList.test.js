96ca3eb444813da91b291a7c6331a65c487733b8
jihwooon <jihwooon@gmail.com>
2022-12-14 22:48:17 +0900

    이중 연결 리스트 get 메서드를 생성하라
    위치에 따라 이중 연결 목록의 노드에 액세스 합니다.
    
    get 의사코드
    1. 인덱스가 0보다 작거나 길이보다 크거나 같으면 null을 반환합니다.
    2. 인덱스가 목록 길이의 절반보다 작거나 같은 경우 입니다.
     - 헤드에서 시작하여 목록을 반복하고 중간을 향해 반복합니다.
     - 노드가 발견되면 반환 합니다.
    3. 인덱스가 목록 길이의 절반보다 큰 경우 입니다.
     - 꼬리에서 시작하여 목록을 반복하고 중간을 향해 반복합니다.
     - 노드가 발견되면 반환 합니다.

--- LinkedList/doubly_linkedList.test.js
+++ LinkedList/doubly_linkedList.test.js
@@ -67,6 +67,27 @@ class DoublyLinkedList {
         this.length++;
         return this;
     }
+
+    get(index) {
+        if(index < 0 | index >= this.length) return null;
+        if(index <= this.length/2) {
+            let count = 0;
+            let current = this.head;
+            while (count != index) {
+            current = current.next;
+            count++;
+        }
+        return current;
+        } else {
+            let count = this.length - 1;
+            let current = this.tail;
+            while(count !== index) {
+                current = current.prev;
+                count--;
+            }
+            return current;
+        }
+    }
 }
 
 let list = new DoublyLinkedList();
