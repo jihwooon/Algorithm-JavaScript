51fde1ead412df7dce80ba3a9b5717374e33b754
jihwooon <jihwooon@gmail.com>
2022-12-14 20:35:38 +0900

    이중 연결 리스트 shift 메서드를 생성하라
    이중 연결 목록의 시작 부분에서 노드 제거 합니다.
    
    shift 의사코드
    1. 길이가 0이면 undefined를 반환합니다.
    2. 현재 헤드 속성을 변수에 저장합니다(이전 헤드라고 함)
    3. 만약 길이가 1이면 다음과 같습니다.
     - head에 null를 저장합니다.
     - tail에 null를 저장합니다.
    4. 헤드를 이전 헤드의 다음 헤드로 업데이트 합니다.
    5. 헤드의 prev 속성을 null로 설정 합니다.
    6. 이전 헤드를 null 옆에 설정 합니다.
    7. 길이 감소 합니다.
    8. 이전 머리를 반환 합니다.

--- LinkedList/doubly_linkedList.test.js
+++ LinkedList/doubly_linkedList.test.js
@@ -41,11 +41,24 @@ class DoublyLinkedList {
         this.length--;
         return poppedNode;
     }
+
+    shift(){
+        if(this.length === 0) return undefined;
+        let oldHead = this.head;
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        }
+        this.head = oldHead.next;
+        this.head.prev = null;
+        this.length--;
+        return oldHead;
+    }
 }
 
 let list = new DoublyLinkedList();
 
 list.push(100);
 list.push(200);
-list.pop();
+list.shift();
 console.log(list);
